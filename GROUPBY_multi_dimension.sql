ad67502613ed13e8e211084d3b229725c0d0871f
Terence Patrick <130315086+terence-patrick@users.noreply.github.com>
2023-12-17 22:05:01 -0800

    Update GROUPBY_multi_dimension.sql

--- GROUPBY_multi_dimension.sql
+++ GROUPBY_multi_dimension.sql
@@ -1,10 +1,11 @@
 select
 	rating,
 	rental_duration,
-    replacement_cost,
-    count(title) AS films_with_this_rental_duration
-from film
+	replacement_cost,
+	count(title) AS films_with_this_rental_duration
+from 
+	film
 group by 
 	rating,
-    replacement_cost,
-    rental_duration;
+    	replacement_cost,
+    	rental_duration;
